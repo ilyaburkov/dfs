53b3f22021bc4c1079c9e5512dc4aa007465dd31
ilyaburkov <s23b_burkov@179.ru>
2021-07-23 16:34:46 +0300

    updated code

--- src/main/java/org/example/dfss/DFS.java
+++ src/main/java/org/example/dfss/DFS.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 
 
 class DFS {
+    public static int[][] gr;
+    public static int[] used;
+    public static int col = 1;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt(), m = in.nextInt();
-        int[][] gr = new int[n][0];
+        gr = new int[n][0];
+        used = new int[n];
+        Arrays.fill(used, 0);
         for (int i = 0; i < m; ++i) {
             int x = in.nextInt(), y = in.nextInt();
             --x;
@@ -16,18 +22,11 @@ class DFS {
             gr[x] = addElement(gr[x], y);
             gr[y] = addElement(gr[y], x);
         }
-        int[] used = new int[n];
-        Arrays.fill(used, 0);
-        int cnt = 1;
-        int[] ans = new int[0];
-        int[] all = {0};
         for(int i = 0;i<n;++i){
             if(used[i] == 0)
             {
-                dfs(i, cnt, all, used, gr);
-                ans = addElement(ans, all[0]);
-                all[0] = 0;
-                cnt++;
+                dfs(i);
+                col++;
             }
         }
         int q = in.nextInt();
@@ -37,20 +36,22 @@ class DFS {
             --b;
             if(used[a] != used[b])
             {
-                System.out.println(0);
+                System.out.println("Эти две вершины не связаны");
             }
-            else
-                System.out.println(ans[used[a] - 1]);
+            else {
+                System.out.println("Эти две вершины связаны");
+            }
         }
     }
-    public static void dfs(int v, int col, int[] all, int[] used, int[][] gr){
+
+    public static void dfs(int v){
         used[v] = col;
-        all[0]++;
         for(int u : gr[v]){
             if(used[u] == 0)
-                dfs(u, col, all, used, gr);
+                dfs(u);
         }
     }
+
     public static int[] addElement(int[] a, int x) {
         int[] res = Arrays.copyOf(a, a.length + 1);
         res[res.length - 1] = x;
