f51ee644aef83dbf75f7c2a072792cf2bcf0c427
ilyaburkov <s23b_burkov@179.ru>
2021-07-23 16:22:24 +0300

    added main algorithm

--- src/main/java/org/example/dfss/DFS.java
+++ src/main/java/org/example/dfss/DFS.java
@@ -1,8 +1,10 @@
+package org.example.dfss;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 
-class dfs {
+class DFS {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt(), m = in.nextInt();
